bb9f00348068a2f3b33ce880299123d7a5f59372
jessica almeida de oliveira <>
2023-04-03 20:14:00 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+struct list{
+int numero;
+struct list *list;
+};
+
+typedef struct list numero;
+
+int main(void){
+  void list(int numero);{
+    insere(1);
+    insere(2);
+    insere(3);
+    insere(7);
+    insere(9);
+    printf("adiciona os numero nesta lista ");
+  };
+  void imprime(int numero);{
+    list(9);
+    list(7);
+    list(3);
+    list(2);
+    list(1);
+    printf("imprime os numero desta list");
+  };
+  void adiciona(int numero);{
+     adiciona(8);
+  }
+  void imprime(int numero);{
+    list(3);
+    list(2);
+    list(1);
+    printf("imprime os numero");
+  };
+  void remover(int numero);{
+    remover(0);
+  printf("list fazia ");
+  };
+  
+  
+
+  return 0;
+};
